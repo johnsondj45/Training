e6f5a40eae211f1cca5d7dccea61af95134204ca
Dylan Johnson <johnsondj45@gmail.com>
2024-09-28 16:02:26 -0400

    Created toast to diagnose issue with setOnClickListener & fixed issue

--- app/src/main/java/com/example/training/MainActivityViewModel.kt
+++ app/src/main/java/com/example/training/MainActivityViewModel.kt
@@ -4,5 +4,12 @@ import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
     var kiki: Cats = Cats("Kiki", "Domestic Shorthair", "Dilly", 8)
-    var meatbol: Cats = Cats("Meatbol", "Domestic Shorthair", "Drew", 8 )
+    var meatbol: Cats = Cats("Meatbol", "Domestic Shorthair", "Drew", 8)
+    fun getNames(name: String): String {
+        return if (name == kiki.name) {
+            meatbol.name
+        } else {
+            kiki.name
+        }
+    }
 }
--- app/src/main/java/com/example/training/fragments/MainFragment.kt
+++ app/src/main/java/com/example/training/fragments/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.training.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.training.MainActivityViewModel
 import com.example.training.R
@@ -11,19 +12,16 @@ class MainFragment : Fragment(R.layout.main_layout) {
     private lateinit var binding: MainLayoutBinding
     private var viewModel = MainActivityViewModel()
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val toast = Toast.makeText(requireActivity(), "Button Clicked", Toast.LENGTH_SHORT)
         binding = MainLayoutBinding.bind(view).apply {
-            textView.text = viewModel.kiki.name
             button.setOnClickListener {
-                textView.text = when (textView.text) {
-                    viewModel.kiki.name -> viewModel.meatbol.name
-                    else -> viewModel.kiki.name
-                }
+                toast.show()
+                textView.text = viewModel.getNames(textView.text.toString())
             }
         }
-    }
 
+    }
 }
 
